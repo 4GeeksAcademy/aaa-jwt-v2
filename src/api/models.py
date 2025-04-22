b113007efc3d29ce63fca6b2d5d592ddfbd564b4
Armando <164431619+Armando7101@users.noreply.github.com>
2025-04-22 20:53:25 +0000

    primer commit

--- src/api/models.py
+++ src/api/models.py
@@ -7,13 +7,15 @@ db = SQLAlchemy()
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=True)
 
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "email": self.email
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,8 +5,11 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 
 api = Blueprint('api', __name__)
+
+bcrypt = Bcrypt()
 
 # Allow CORS requests to this API
 CORS(api)
@@ -20,3 +23,43 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/user', methods=['POST'])
+def create_user():
+
+    name = request.json.get('name')
+    password = request.json.get('password')
+    email = request.json.get('email')
+    
+    if not name or not password or not email: 
+        
+        return jsonify({ 'msg': 'necesitamos todos los datos para poder registrar un usuario' }), 400
+    
+    user = User.query.filter_by(email = email).first()
+
+    if user: 
+        return jsonify({ 'msg': 'este email ya esta registrado' }), 400
+    
+    password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    new_user = User(name = name, password = password_hash, email = email)
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(new_user.serialize()), 201
+
+@api.route('/login', methods=['POST'])
+def login():
+    
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not email or not password:
+        return jsonify({'msg': 'error en email o password'}), 400
+
+    current_user = User.query.filter_by(email = email).first()
+    if not current_user:
+        return jsonify({'msg': 'usuario no existe'}), 404
+    
+    #
